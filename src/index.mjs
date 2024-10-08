e3715c43df54b648260070d3f5725e1aae0d92dc
KevinMolinuevo <132021342+KevinMolinuevo@users.noreply.github.com>
2024-10-08 20:49:18 +0000

    First session

--- src/index.mjs
+++ src/index.mjs
@@ -2,8 +2,40 @@ import express from "express";
 
 const app =express();
 
-const PORT =process.env.PORT || 300
+const PORT =process.env.PORT || 3000;
+
+const mockUsers = [
+    {id:1, username: "anson", displayName: "Anson"},
+    {id:2, username: "jack", displayName: "Jack"},
+    {id:3, username: "adam", displayName: "Adam"},
+]
+
+app.get("/", (request, response)=> {
+    response.status(201).send({ msg: "Hello"});
+});
+
+app.get('/api/users', (request, response)=> {
+    response.send(mockUsers);
+});
+
+app.get('/api/users/:id', (request, response)=>{
+    console.log(request.params);
+    const parsedId= parseInt(request.params.id);
+    console.log(parsedId);
+    if (isNaN(parsedId))
+        return response.status(400).send({msg : "Bad Request>"});
+
+    const findUser = mockUsers.find((user)=> user.id=== parsedId);
+    if (!findUser) return response.sendStatus(404);
+    return response.send(findUser);
+});
+
+
+app.get('/api/products', (request, response)=>{
+    response.send([{id: 123, name: "chicken breast", price: 12.99}]);
+});
 
 app.listen(PORT, ()=> {
     console.log(`Running on Port ${PORT}`);
 });
+
