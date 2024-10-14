94752b71d297961312d9cda892484953e6e8c36f
KevinMolinuevo <132021342+KevinMolinuevo@users.noreply.github.com>
2024-10-14 20:40:50 +0000

    cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,23 +1,23 @@
 import express from "express";
-import routes from "./routes/index.mjs"
+import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app =express();
 
 app.use(express.json());
+app.use(cookieParser("helloworld"));
 app.use(routes);
 
 const PORT =process.env.PORT || 3000;
 
-
-app.get("/", 
-    (request, response, next)=>{
-        console.log("Base URL");
-        next();
-    },
-    (request, response) => {
-        response.status(201).send({ msg: "Hello"});
-});
-
 app.listen(PORT, ()=> {
     console.log(`Running on Port ${PORT}`);
 });
+
+app.get("/", 
+    (request, response) => {
+        response.cookie("hello", "world", {maxAge: 30000, signed:true });
+        response.status(201).send({ msg: "Hello" });
+    });
+       
+
